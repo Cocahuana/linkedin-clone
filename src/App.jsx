483faa71469c249c021b11cab4042f8413f825ff
Ezequiel <ezequieldominguez11@gmail.com>
2022-10-23 11:58:07 -0300

    Feat: components, pages and examples added. Used Children inside Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import getTestingText from "./services/actions/cart/index";
-
+import Home from "./Pages/Home/Home";
 function App() {
 	const dispatch = useDispatch();
 
@@ -18,6 +18,7 @@ function App() {
 	return (
 		<div className='App'>
 			{existText ? <h2>{existText}</h2> : <h2>Nop</h2>}
+			<Home />
 		</div>
 	);
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Wrapper from "../../Components/Wrapper/Wrapper";
+import MenuExample from "../../Components/MenuExample/MenuExample";
+function Home() {
+	return (
+		<Wrapper>
+			<MenuExample />
+		</Wrapper>
+	);
+}
+
+export default Home;
